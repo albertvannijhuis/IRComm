2652af3285576f756f5cabed2f7bbbb21fc317ac
Unknown <albertvannijhuis@gmail.com>
2018-12-03 15:26:10 +0100

    hoi

--- src/IRComm.h
+++ src/IRComm.h
@@ -33,39 +33,8 @@
 
 class IRComm
 {
-  public:
-
-  protected:
-
-  private:
-	// Defining variables
-
-<<<<<<< refs/remotes/origin/master
-	// IF the frequency of the PWM signal should be 38kHz
-<<<<<<< refs/remotes/origin/master
-	#if PWMFREQ == 38
-		const uint8_t IRComm::top = 26;
-		uint8_t recTimerOverflow = 0;
-	#elif PWMFREQ == 56
-		const uint8_t top = 16;
-		uint8_t recTimerOverflow = 0;
-	#else
-	#error Invalid PWM Frequency
-	#endif
-=======
-#if PWMFREQ == 38
-	uint8_t IRComm::top = TOP_38KHZ;
-	uint8_t recTimerOverflow = 0;
-#elif PWMFREQ == 56
-	uint8_t top = TOP_56KHZ;
-	uint8_t recTimerOverflow = 0;
-#else
-#error Invalid PWM Frequency
-#endif
-=======
-    // IF the frequency of the PWM signal should be 38kHz
-
-    
+    public:
+    // Defining variables
     #if PWMFREQ == 38
     uint8_t top = TOP_38KHZ;
     uint8_t recTimerOverflow = 0;
@@ -77,63 +46,21 @@ class IRComm
     #else
     #error Invalid PWM Frequency
     #endif
->>>>>>> hoii
->>>>>>> hoii
 
-	// Defining functions
-	void initSendTimer();
-	void initRecTimer();
-	void initReceive();
-<<<<<<< refs/remotes/origin/master
-=======
+    uint8_t step;
+    uint8_t pulseCounter;
+    uint16_t counter;
+    uint8_t pulseTimerOn = false;
+
+    // Defining functions
+    void initSendTimer();
+    void initRecTimer();
+    void initReceive();
 
 
-  protected:
-
-<<<<<<< refs/remotes/origin/master
-  private:
->>>>>>> hoii
-}
-
-// Defining includes
-#include <Arduino.h>
-#include <avr/io.h>
-#include <util/delay.h>
-
-// Defining used pins
-#define LED_PIN PORTB6
-#define PCI_PIN PORTD4
-
-// Defining macros for the ledpin
-#define ENABLE_LEDPIN PORTB |= (1 << LED_PIN)
-#define DISABLE_LEDPIN PORTB &= ~(1 << LED_PIN)
-#define TOGGLE_LEDPIN PORTB ^= (1 << LED_PIN)
-
-// Defining macros for the Pin Change Interrupt pin
-#define RCPIN_HIGH !(PORTD & (1 << PCI_PIN))
-
-// Defining pulse spaces between bits
-#define START_BIT 80
-#define STOP_BIT 60
-#define ONE_BIT 40
-#define ZERO_BIT 20
-
-<<<<<<< refs/remotes/origin/master
-// Set frequency in kHz
-#define PWMFREQ 38
-=======
-// Defining constants
-#define TOP_38KHZ 26
-#define TOP_56KHZ 16
-=======
     protected:
 
     private:
 };
 
 #endif
->>>>>>> hoii
-
-
->>>>>>> hoii
-
